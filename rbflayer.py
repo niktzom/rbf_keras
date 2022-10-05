c6fcfbb9fa976edf7c36b8915c8740fb3d656323
niktzom <niktzo99@gmail.com>
2022-10-05 19:30:30 +0300

    Update rbflayer.py

--- rbflayer.py
+++ rbflayer.py
@@ -17,14 +17,15 @@ class InitCentersRandom(Initializer):
         self.X = X
 	
     def __call__(self, shape, dtype=None):
-	assert shape[1] == self.X.shape[1]
-	idx = np.random.randint(self.X.shape[0], size=shape[0])
-	
-	#type checking to access elements of data correctly
-	if type(self.X) == np.ndarray:
-		return self.X[idx, :]
-	elif type(self.X) == pd.core.frame.DataFrame:
-		return self.X.iloc[idx, :]
+        assert shape[1] == self.X.shape[1]
+        idx = np.random.randint(self.X.shape[0], size=shape[0])
+        #type checking to access elements of data correctly
+
+        if type(self.X) == np.ndarray:
+            return self.X[idx, :]
+        elif type(self.X) == pd.core.frame.DataFrame:
+            return self.X.iloc[idx, :]
+   
 
 class RBFLayer(Layer):
     """ Layer of Gaussian RBF units.
